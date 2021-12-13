4504629d066a85a3b9141f85fa1adc35a0b4603b
denisesmelich <56693379+denisesmelich@users.noreply.github.com>
2021-12-13 09:01:24 -0800

    Add initial files for CodeExchange

---
+++ 2_xfpl_xfnl_web_service/WebApp_CS/WebService.asmx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using spc2011;
+
+namespace WebApp_CS
+{
+    /// <summary>
+    /// Summary description for WebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+
+    public class WebService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public List<Customer> GetAllCustomers()
+        {
+            SynergyServer svr = new SynergyServer();
+            List<Customer> customers = new List<Customer>();
+            svr.GetAllCustomers(ref customers);
+            return customers;
+        }
+
+        [WebMethod]
+        public List<Contact> GetCustomerContacts(int CustomerId)
+        {
+            SynergyServer svr = new SynergyServer();
+            List<Contact> contacts = new List<Contact>();
+            svr.GetCustomerContacts(CustomerId, ref contacts);
+            return contacts;
+        }
+
+        [WebMethod]
+        public Method_status GetContactForUpdate(int customerId, int contactId, out Contact contact, out byte[] grfa, out String msg)
+        {
+            SynergyServer svr = new SynergyServer();
+            contact = new Contact();
+            grfa = new byte[10];
+            msg = "";
+            return svr.GetContactForUpdate(customerId, contactId, ref contact, ref grfa, ref msg);
+        }
+
+        [WebMethod]
+        public Method_status UpdateContact(ref Contact contact, ref byte[] grfa, out String msg)
+        {
+            SynergyServer svr = new SynergyServer();
+            msg = "";
+            return svr.UpdateContact(ref contact, ref grfa, ref msg);
+        }
+
+    }
+}
